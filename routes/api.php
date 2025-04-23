44e3035a1f7547a8cce714a853fc58bc4a3e424a
Gabriel Mioni <104784563+gab3mioni@users.noreply.github.com>
2025-04-23 15:43:38 -0300

    feat: criando rota hello-world
    apenas pra verificar se a API está funcionando

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TemplateController;
 
 Route::post('/enviar-template', [TemplateController::class, 'enviar']);
+Route::get('/hello-world', function () {
+    return response()->json(['message' => 'Hello World']);
+});

99d4d76573299f517d5393ef2a3cf89b1903de33
Gabriel Mioni <104784563+gab3mioni@users.noreply.github.com>
2025-04-29 16:39:03 -0300

    test: criando teste para despachar jobs para cada template

--- tests/Feature/Console/Commands/VerificarTemplatesTest.php
+++ tests/Feature/Console/Commands/VerificarTemplatesTest.php
@@ -44,3 +44,20 @@ it('exibe mensagem se não houver templates aguardando aprovação', function ()
 
     Queue::assertNothingPushed();
 });
+
+it('despacha jobs para cada template pendente', function () {
+    $template1 = Template::factory()->create(['status' => 'WAITING_APROVAL']);
+    $template2 = Template::factory()->create(['status' => 'WAITING_APROVAL']);
+
+    $this->artisan('verificar:templates')
+        ->expectsOutput('Jobs para verificação de templates foram enviados para a fila.')
+        ->assertExitCode(0);
+
+    Queue::assertPushed(VerificarTemplateStatusJob::class, 2);
+    Queue::assertPushed(VerificarTemplateStatusJob::class, function ($job) use ($template1) {
+        return $job->getTemplate()->id === $template1->id;
+    });
+    Queue::assertPushed(VerificarTemplateStatusJob::class, function ($job) use ($template2) {
+        return $job->getTemplate()->id === $template2->id;
+    });
+});
